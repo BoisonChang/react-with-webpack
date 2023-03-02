27a7dd1888729abc9344b8fcf6c653033620320e
柏崧張 <baisongzhang@baisongzhangdeMacBook-Pro.local>
2023-03-02 18:17:35 +0800

    feat: set webpack.config.js to read jsx

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default function App() {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    )
-}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js'
@@ -15,7 +15,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /.js$/,
+                test: /.jsx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
@@ -28,5 +28,8 @@ module.exports = {
                 }
             }
         ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
     }
 }
