ae7cda47e3a7c8265ebd3d705052721d47fae361
柏崧張 <baisongzhang@baisongzhangdeMacBook-Pro.local>
2023-03-02 17:02:45 +0800

    feat: component by function

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,9 @@
 import React, { Component } from "react";
 
-// export default function App() {
-//     return (
-//         <div>
-//             <h1>Hello World!</h1>
-//         </div>
-//     )
-// }
-
-
-class App extends Component {
-    render() {
-        return(
-            <div>
-                <h1>Hello World!</h1>
-            </div>   
-        )
-    }
+export default function App() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
 }
-
-export default App;
