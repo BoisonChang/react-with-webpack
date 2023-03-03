ed305c89f9ab4a128f6d639c5e04fdeec7b521e3
柏崧張 <baisongzhang@baisongzhangdeMacBook-Pro.local>
2023-03-03 11:34:07 +0800

    feat: make webpack to resolve typescript

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styles from "./App.scss";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className={styles.h1}>Hello World!</h1>
+    </div>
+  );
+}
